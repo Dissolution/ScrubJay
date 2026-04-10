20e06dc7ce3ebf3aa100c0d3226dd38ae5e2f45e
Timothy Jay <TimothyRJay@gmail.com>
2026-04-10 11:59:23 -0700

    checkpoint

--- Sandboxes/Console/Program.cs
+++ Sandboxes/Console/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ScrubJay.Enums;
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+
+
+
+
+
+Console.WriteLine("Press enter to close this Sandbox.");
+Console.ReadLine();
+return;
+
+
+
+internal partial class Util
+{
+    public static void EnumThing<E>(E e)
+        where E : struct, Enum
+    {
+        e.Format(EnumPart.Attribute, )
+    }
+}
